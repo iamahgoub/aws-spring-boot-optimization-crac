37a7c6c74c015b8db849acf6a24bd55c2a7fbcac
Islam M <mahgisla@amazon.co.uk>
2023-07-02 11:26:00 +0100

    updating k8s manifests/clean-up

--- baseline/CustomerService/bin/scripts/run-service.sh
+++
@@ -1,6 +0,0 @@
-# run java
-java -XX:CRaCRestoreFrom=/opt/crac-files
-
-# the code below is to keep the container running if the java process failed 
-echo Executing an infinite loop to keep the container running...
-while true; do sleep 1; done
